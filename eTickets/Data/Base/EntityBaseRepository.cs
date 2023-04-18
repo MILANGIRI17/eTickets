3ff7f4eb43cc1dae341e0f91bd1c2b7655b5c68a
Milan Giri <milan.giri17021999@gmail.com>
2023-04-18 13:11:49 +0545

    entity base repository savechanges

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -11,26 +11,28 @@ public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class,
     {
         this.context = context;
     }
-    public Task AddAsync(T entity)
+    public async Task AddAsync(T entity)
     {
-        throw new NotImplementedException();
+        await context.Set<T>().AddAsync(entity);
+        await context.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(int id)
     {
-        var entity=await context.Set<T>().FirstOrDefaultAsync(x=>x.Id==id);
-        EntityEntry entityEntry =  context.Entry<T>(entity);
+        var entity = await context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+        EntityEntry entityEntry = context.Entry<T>(entity);
         entityEntry.State = EntityState.Deleted;
+        await context.SaveChangesAsync();
     }
 
-    public async Task<IEnumerable<T>> GetAllAsync()=> await context.Set<T>().ToListAsync();
+    public async Task<IEnumerable<T>> GetAllAsync() => await context.Set<T>().ToListAsync();
 
-    public async Task<T> GetByIdAsync(int id)=> await context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+    public async Task<T> GetByIdAsync(int id) => await context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
 
     public async Task UpdateAsync(int id, T entity)
     {
         EntityEntry entityEntry = context.Entry<T>(entity);
         entityEntry.State = EntityState.Modified;
+        await context.SaveChangesAsync();
     }
 }
- 
